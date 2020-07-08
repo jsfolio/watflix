a79916d69638c8eb53c61e5d37a20169cbcb96f8
hyemin <jhm8540@daum.net>
2020-07-09 00:06:19 +0900

    200708

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect , Switch} from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
 
 export default () => (
     <Router>
-        <>
-            <Route path="/" axact component={Home}/>
-            <Route path="/tv" axact component={TV}/>
-            <Route path="/search" axact component={Search}/>
-        </>
+        <Switch>
+            <Route path="/" axact component={Home} />
+            <Route path="/tv" axact component={TV} />
+            <Route path="/tv/popular" render = { () => <h1>Popular</h1> } />
+            <Route path="/search" component={Search} /> 
+            <Redirect from="*" to="/" />
+        </Switch>
     </Router>
 )
