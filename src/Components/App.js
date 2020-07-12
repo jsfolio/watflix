7f204f0ca602b2322acc4ab3a6cd83657ef575ee
hyemin <jhm8540@daum.net>
2020-07-13 05:16:49 +0900

    헤더 CSS

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Router from "Components/Router";
-import Header from "Components/Header"
-class App extends Component{
-  render(){
+import GlobalStyles from "Components/GlobalStyles"
+class App extends Component {
+  render() {
     return (
       <>
-        <Header />
         <Router />
+        <GlobalStyles/>
       </>
     );
   }
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect , Switch} from "react-router-dom";
+import { 
+    BrowserRouter as Router, 
+    Route, 
+    Redirect , 
+    Switch
+} from "react-router-dom";
+import Header from "Components/Header"
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
 
 export default () => (
     <Router>
+        <>
+        <Header/>
         <Switch>
-            <Route path="/" axact component={Home} />
-            <Route path="/tv" axact component={TV} />
+            <Route path="/" exact component={Home} />
+            <Route path="/tv" exact component={TV} />
             <Route path="/tv/popular" render = { () => <h1>Popular</h1> } />
             <Route path="/search" component={Search} /> 
             <Redirect from="*" to="/" />
         </Switch>
+        </>
     </Router>
-)
+);
